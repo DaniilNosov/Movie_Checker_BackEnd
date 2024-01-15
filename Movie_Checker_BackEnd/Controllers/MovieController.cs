97bf7ac777a32048f810a78f0af77e39985889f8
Daniil <Daniil@DESKTOP-06A6QOM>
2024-01-15 11:19:55 +0200

    Добавьте файлы проекта.

---
+++ Movie_Checker_BackEnd/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movie_Checker_BackEnd.Services;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Movie_Checker_BackEnd.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class MovieController : ControllerBase
+{
+    private readonly CheckerService _service;
+
+    public MovieController(CheckerService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetMovies(string movieName)
+    {
+        var jsonDataString = await _service.CheckMovieAsync(movieName);
+        return Ok(jsonDataString);
+    }
+}
---
+++ Movie_Checker_BackEnd/Services/CheckerService.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace Movie_Checker_BackEnd.Services;
+
+public class CheckerService
+{
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
+
+    public CheckerService(HttpClient httpClient, IConfiguration configuration)
+    {
+        _httpClient = httpClient;
+        _configuration = configuration;
+    }
+
+    public async Task<string> CheckMovieAsync(string movieName)
+    {
+        var apiKey = _configuration["ApiKey"];
+        var encodedName = WebUtility.UrlEncode(movieName);
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"https://ott-details.p.rapidapi.com/search?title={encodedName}&page=1"),
+            Headers =
+    {
+        { "X-RapidAPI-Key", $"{apiKey}" },
+        { "X-RapidAPI-Host", "ott-details.p.rapidapi.com" },
+    },
+        };
+        using (var response = await _httpClient.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            return body;
+        }
+    }
+}
